8db87736d06294af8c5f7cbd99abc8454f330d41
Nisargg007 <104688398+Nisargg007@users.noreply.github.com>
2022-07-31 15:34:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,133 @@
 
+resource "aws_vpc" "myVPC" {
+  cidr_block                       = var.cidr
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_dns_support               = var.enable_dns_support
+  tags = {
+    Name = var.vpc_name
+  }
+}
+resource "aws_internet_gateway" "myIGW" {
+
+  vpc_id = aws_vpc.myVPC.id
+  tags = {
+    "Name" = var.igw_tag
+  }
+}
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                          = aws_vpc.myVPC.id
+  cidr_block                      = var.public_subnets_cidr_1
+  availability_zone               = data.aws_availability_zones.available_1.names[0]
+  map_public_ip_on_launch         = var.map_public_ip_on_launch
+
+  tags = {
+   Name = var.public_subnet_tag_1
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                          = aws_vpc.myVPC.id
+  cidr_block                      = var.public_subnets_cidr_2
+  availability_zone               = data.aws_availability_zones.available_1.names[1]
+  map_public_ip_on_launch         = var.map_public_ip_on_launch
+
+  tags = {
+   Name = var.public_subnet_tag_2
+  }
+}
+esource "aws_subnet" "database_subnet_1" {
+  vpc_id                          = aws_vpc.myVPC.id
+  cidr_block                      = var.database_subnets_cidr_1
+  availability_zone               = data.aws_availability_zones.available_1.names[0]
+  map_public_ip_on_launch         = false
+
+  tags = {
+    Name = var.database_subnet_tag_1
+  }
+}
+resource "aws_subnet" "database_subnet_2" {
+  vpc_id                          = aws_vpc.myVPC.id
+  cidr_block                      = var.database_subnets_cidr_2
+  availability_zone               = data.aws_availability_zones.available_1.names[1]
+  map_public_ip_on_launch         = false
+
+  tags = {
+    Name = var.database_subnet_tag_2
+  }
+}
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.myVPC.id
+  tags = {
+    Name = var.public_route_table_tag
+  }
+}
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.myIGW.id 
+}
+resource "aws_route_table" "database_route_table" {
+  vpc_id = aws_vpc.myVPC.id
+
+  tags = {
+    Name = var.database_route_table_tag
+  }
+}
+ 
+resource "aws_route_table_association" "public_route_table_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_route_table_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+resource "aws_route_table_association" "database_route_table_association_1" {
+  subnet_id      = aws_subnet.database_subnet_1.id
+  route_table_id = aws_route_table.database_route_table.id
+}
+resource "aws_route_table_association" "database_route_table_association_2" {
+  subnet_id      = aws_subnet.database_subnet_2.id
+  route_table_id = aws_route_table.database_route_table.id
+}
+resource "aws_security_group" "sg" {
+  name        = "Nisarg_security_group"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.myVPC.id
+
+  ingress = [
+    {
+      description      = "All traffic"
+      from_port        = 0    
+      to_port          = 0    
+      protocol         = "-1" 
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      description      = "Outbound rule"
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  tags = {
+    Name = "Nisarg_security_group"
+  }
+}
+
+
